e4061840741dc19f50b6c05b0279bd54ba86d6d2
ndcnf <chat.cheshire@gmail.com>
2022-06-02 17:15:15 +0200

    Makefile added, files ready to accept decent code

---
+++ philo/philo.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 17:13:47 by nchennaf          #+#    #+#             */
+/*   Updated: 2022/06/02 17:13:51 by nchennaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> // printf
+#include <unistd.h> // write, usleep
+#include <string.h> // memset
+#include <stdlib.h> // malloc, free
+#include <sys/time.h> // gettimeofday
+#include <pthread.h> // pthread_*
+
