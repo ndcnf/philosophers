09168721747362fa07e9e03a468b9cb1f360fc15
ndcnf <chat.cheshire@gmail.com>
2022-06-06 11:05:56 +0200

    Makefile updated (+ hello world for test)

--- philo/philo.c
+++ philo/philo.c
@@ -6,8 +6,16 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:14:15 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/06/02 17:14:18 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/06/06 11:04:43 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("hello world\n");
+	return (0);
+}
