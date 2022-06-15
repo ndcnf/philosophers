c42ef3b797f79bce6cb246414e2a9d00e73c45cf
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-06-15 16:17:57 +0200

    need_space ok

--- philo/init.c
+++ philo/init.c
@@ -6,18 +6,28 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 10:33:16 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/06/15 14:55:05 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:17:20 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	need_space(t_philos *phis)
+void	need_space(t_philos *phis, int n_phis)
 {
-	phis->in = malloc(sizeof(t_inputs));
-	// phis = malloc(sizeof(t_philos) * phis->in->number_of_philosophers);
-	// 	if (!phis)
-	// 		errorminator(ERR_MEM);
+	int	i;
+
+	i = 0;
+	phis = malloc(sizeof(t_philos) * n_phis);
+	if (!phis)
+		errorminator(ERR_MEM);
+	while(i < n_phis)
+	{
+		phis[i].in = malloc(sizeof(t_inputs));
+		if (!phis[i].in)
+			errorminator(ERR_MEM);
+		init_args(&phis[i]);
+		i++;
+	}
 }
 
 void	init_args(t_philos *phis)
--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 17:14:15 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/06/15 15:48:10 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:17:43 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 int	main(int argc, char *argv[])
 {
-	t_philos	phis;
+	t_philos	*phis;
 
+	phis = NULL;
 	check_args(argc, argv);
-	need_space(&phis);
-	init_args(&phis);
-	args_manager(&phis, argc, argv);
+	need_space(phis, ft_atoi(argv[1]));
+	// args_manager(&phis, argc, argv);
 
-	pthread_create(&phis.phi, NULL, philo_starter_pack, (void *)&phis);
-	while (1)
-	{
-		usleep(200);
-		printf("Polo !\n");
-	}
-	time_usec();
+	// pthread_create(&phis.phi, NULL, philo_starter_pack, (void *)&phis);
+	// while (1)
+	// {
+	// 	usleep(200);
+	// 	printf("Polo !\n");
+	// }
+	// time_usec();
 
 	return (EXIT_SUCCESS);
 }
