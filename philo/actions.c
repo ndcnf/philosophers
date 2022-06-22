a4cf71424e32d76bab6cf2f1e3cab9c3ffb29f1f
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-06-22 17:21:37 +0200

    wip, testing mutex

--- philo/actions.c
+++ philo/actions.c
@@ -6,30 +6,24 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 09:52:02 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/06/22 15:53:31 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/06/22 17:02:00 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// int		eat_something(t_philos *phi)
-// {
-// 	// int	i;
-
-// 	// i = 0;
-
-// 	// while (pthread_mutex_lock(&phi->fork) && pthread_mutex_lock(&phi->fork))
-// 	// {
-// 	// 		printf("j'ai deux fourchettes, yeah\n");
-// 	// 	pthread_mutex_unlock(&phi->fork);
-// 	// }
-// 	// pthread_mutex_unlock(&phi->neighbour->fork);
-
-// 	// 	while (i < phi->in->time_to_eat)
-// 	// 	{
-
-// 	// 	}
-// 	// return (EXIT_SUCCESS);
+int		eat_something(t_philos *phi)
+{
+	while (1)
+	{
+		pthread_mutex_lock(&phi->in->fork[phi->id]);
+		printf("this is MY fork\n");
+		pthread_mutex_lock(&phi->in->fork[phi->neighbour]);
+		printf("I borrow YOUR fork\n");
+		printf("now I can eat\n");
+		pthread_mutex_unlock(&phi->in->fork[phi->neighbour]);
+		pthread_mutex_unlock(&phi->in->fork[phi->id]);
+	}
 
 
 
@@ -37,6 +31,66 @@
 
 
 
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// int	i;
+
+	// i = 0;
+
+	// while (pthread_mutex_lock(&phi->fork) && pthread_mutex_lock(&phi->fork))
+	// {
+	// 		printf("j'ai deux fourchettes, yeah\n");
+	// 	pthread_mutex_unlock(&phi->fork);
+	// }
+	// pthread_mutex_unlock(&phi->neighbour->fork);
+
+	// 	while (i < phi->in->time_to_eat)
+	// 	{
+
+	// 	}
+	return (EXIT_SUCCESS);
+
+
+
+
+
+
+
+}
 
 
--- philo/inputs.c
+++ philo/inputs.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 08:50:51 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/06/22 16:12:39 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/06/22 17:06:01 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	args_manager(t_inputs *in, int argc, char *argv[])
 	in->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
 		in->nbr_of_meals = ft_atoi(argv[5]);
+	else
+		in->nbr_of_meals = 0;
 }
 
 int		philo_starter_pack(t_philos **phis)
@@ -38,7 +40,7 @@ int		philo_starter_pack(t_philos **phis)
 		if (pthread_create(&(*phis)[i].phi, NULL, the_routine, &(*phis)[i])) //AVANT (void *)&(*phis)[i] mais trop long. IDEM ?
 			return (errorminator(ERR_THD));
 		// usleep(100);
-		printf("son voisin de droite : [%d][%d]\n", (*phis)[i].id, (*phis)[i].neighbour);
+		// printf("son voisin de droite : [%d][%d]\n", (*phis)[i].id, (*phis)[i].neighbour);
 		i++;
 	}
 	//pthread_join((*phis)[i].phi, NULL); // termine le thread avant de continuer -- CORRIGER
@@ -54,15 +56,16 @@ void	*the_routine(void *arg)
 
 	i = 0;
 	phi = (t_philos *)arg; //Est-ce que c'est un peu plus juste en castant le type attendu?
-	printf("ROUTINE, phi id[%d], AVANT boucle\n\n\n", phi->id);
+	// printf("ROUTINE, phi id[%d], AVANT boucle\n\n\n", phi->id);
 
-	while (i < 3) // le '3' n'est que pour TESTER, cette valeur n'a AUCUN SENS
+	while (i < 10) // le '3' n'est que pour TESTER, cette valeur n'a AUCUN SENS
 	{
-		printf("id[%d], their fork[%p]\n", phi->id, &phi->in->fork[phi->id]);
-		printf("neighbour's id[%d], their neighbour's fork[%p]\n", phi->neighbour, &phi->in->fork[phi->neighbour]);
-		printf("ROUTINE, DANS boucle\n");
+		eat_something(phi);
+		// printf("id[%d], their fork[%p]\n", phi->id, &phi->in->fork[phi->id]);
+		// printf("neighbour's id[%d], their neighbour's fork[%p]\n", phi->neighbour, &phi->in->fork[phi->neighbour]);
+		// printf("ROUTINE, DANS boucle\n");
 		i++;
 	}
-	printf("ROUTINE this one's done, NEXT---------------------------\n\n\n");
+	// printf("ROUTINE this one's done, NEXT---------------------------\n\n\n");
 	return (NULL);
 }
